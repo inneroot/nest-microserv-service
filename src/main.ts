397f698813a422befb4129c46b13f9a852f0784b
inneroot <hakooplay@gmail.com>
2020-11-20 12:42:49 +0300

    add @nestjs/microservices & nats

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+const MicroserviceOptions = {
+  transport: Transport.NATS,
+  options: {
+    url: 'nats://localhost:4222',
+  },
 }
+
+async function bootstrap() {
+  const app = await NestFactory.createMicroservice(AppModule, MicroserviceOptions);
+  app.listen(() => console.log('Microservice is listening...'));
+}
+
 bootstrap();
